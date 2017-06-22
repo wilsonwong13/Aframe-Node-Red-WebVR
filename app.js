1c72a336f6e95cc10c40a2aae39c3b3a55a62d45
Wilson Wong <wilsonwong13@gmail.com>
2017-06-21 23:13:29 -0400

    setting up server and heroku deployement

--- app.js
+++ app.js
@@ -7,4 +7,6 @@ app.get('/', function(req, res, next) {
   res.sendFile(__dirname + '/public/index.html')
 })
 
-app.listen(process.env.PORT)
+app.listen(3000, function() {
+  console.log('listening on port 3000' )
+})
